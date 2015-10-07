647bc3d932a0bfdc1d2634780c3a25c3a28b82f0
Vincenzo De Notaris <vdenotaris@users.noreply.github.com>
2015-10-07 14:18:24 +0200

    Update LandingController.java

--- src/main/java/com/vdenotaris/spring/boot/security/saml/web/contollers/LandingController.java
+++ src/main/java/com/vdenotaris/spring/boot/security/saml/web/contollers/LandingController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Vincenzo De Notaris
+ * Copyright 2015 Vincenzo De Notaris
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
