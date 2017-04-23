8a68cf63c2a83ae4f724cb55ef90f4635216a7ba
vdenotaris <vdenotaris@me.com>
2017-04-23 11:01:42 +0200

    Version update for new release.

--- src/main/java/com/vdenotaris/spring/boot/security/saml/web/Application.java
+++ src/main/java/com/vdenotaris/spring/boot/security/saml/web/Application.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Vincenzo De Notaris
+ * Copyright 2017 Vincenzo De Notaris
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
